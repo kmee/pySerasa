5903618571a905877b2176d1326534736f72966a
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2015-10-22 09:41:57 -0200

    [NEW] setup.py and docs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pySerasa',
+    version='0.1',
+    packages=['pyserasa'],
+    url='https://gitlab.kmee.com.br/python/pySerasa',
+    license='',
+    author='KMEE',
+    author_email='luiz.divino@kmee.com.br',
+    description='Modulo para consulta de string de dados SERASA'
+)
